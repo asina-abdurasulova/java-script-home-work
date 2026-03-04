c050e64a28461bc379da67deff5e06de50741e81
asina-abdurasulova <abdurasulova.as@gmail.com>
2026-03-04 14:28:24 +0600

    Выпонено

---
+++ javascript8/js.js
@@ -0,0 +1,39 @@
+// let age = prompt("Введите свой возраст");
+// console.log(age)
+// "Привет меня зовут Асина мне 21 год я студентка Металабс"
+// Введите ваш еимя
+// введите ваш возраст
+// let name = prompt("Введите свое имя");
+
+// let age = prompt("Введите ваш возраст");
+// // console.log("Привет меня зовут " +name+ " мне " + age + " лет я студент Металабс")
+// console.log(`Привет меня зовут ${name} мне ${age} лет я студент Металабс`)
+
+// let a = 5
+// let c = a + 2 
+// let d = b - a 
+// let d = a + c - ba = 
+// Привет, Асина
+// Абдурасулова (фамилия)
+// вам 21 лет
+// ваш рост 162
+// ваш пол: женский 
+// ваш вес: 58 
+
+let name = prompt("Введите свое имя");
+let surname = prompt("Введите вашу фамилию");
+let age = prompt("Введите ваш возраст");
+let heightHuman = prompt("Введите ваш рост в см");
+let gender = prompt("Введите пол: жен/муж");
+let weightHuman = prompt("Введите ваш вес в кг");
+console.log(`Привет, ${name}`);
+console.log(`${surname}`);
+console.log(`Вам ${age} лет`);
+console.log(`Ваш рост: ${heightHuman} `);
+console.log(`Ваш пол: ${gender}`);
+console.log(`Ваш вес: ${weightHuman}`);
+
+
+
+
+
